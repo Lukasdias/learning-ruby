a01794022d718482ff57e6900dd4e2a5ebaac158
Lukasdias <diaslukas19@gmail.com>
2025-12-21 17:24:58 -0300

    feat: Update caesar_cipher to accept user input for text and shift factor

--- ceaser_cypher.rb
+++ ceaser_cypher.rb
@@ -1,7 +1,13 @@
 $alphabet = ('a'..'z').to_a
 
 def caesar_cipher(text, num)
-  
+  # to implement
 end
 
-puts caesar_cipher("What a string!", 5)
+print "Write the text to be encoded: "
+phrase = gets.chomp()
+
+print "Write the shift factor: "
+shift_factor = gets.chomp()
+
+puts caesar_cipher(phrase, shift_factor)
